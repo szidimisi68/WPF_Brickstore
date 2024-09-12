d72d2905064f367e8e315c7ef39449c8e00aba49
szidimisi68 <70961855+szidimisi68@users.noreply.github.com>
2024-09-12 11:06:28 +0200

    Add files via upload

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,7 @@ namespace WpfApp2
         ObservableCollection<Lego> legok = new();
         List<string> kategoriak = new();
         ObservableCollection<Lego> filtered = new();
+        List<string> fajlok = new();
         public MainWindow()
         {
             InitializeComponent();
@@ -31,30 +34,45 @@ namespace WpfApp2
         private void btnOpen_Click(object sender, RoutedEventArgs e)
         {
             legok.Clear();
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "BSX files (*.bsx)|*.bsx";
+            OpenFolderDialog ofd = new OpenFolderDialog();
+            ofd.Multiselect = false;
+            //ofd.Filter = "BSX files (*.bsx)|*.bsx";
             if (ofd.ShowDialog() == true)
             {
-                XDocument xaml = XDocument.Load(ofd.FileName);
-                foreach (var elem in xaml.Descendants("Item"))
-                {
-                    string id = elem.Element("ItemID").Value;
-                    string nev = elem.Element("ItemName").Value;
-                    string category = elem.Element("CategoryName").Value;
-                    string color = elem.Element("ColorName").Value;
-                    string qt = elem.Element("Qty").Value;
-
-                    legok.Add(new Lego(id, nev, category, color, qt));
-                }
-                dgTablazat.ItemsSource = legok;
-                lbElemek.Content = legok.Count();
-                kategoriak = legok.Select(x => x.CategoryName).Distinct().ToList();
-                kategoriak.Add("none");
-                cbKategoriak.ItemsSource = kategoriak;
-                cbKategoriak.SelectedIndex = kategoriak.Count()-1;
-                filtered = legok;
-               
+                fajlok = Directory.GetFiles(ofd.FolderName).ToList();
             }
+            lbFolder.Content = ofd.FolderName.ToString();
+            lbxList.ItemsSource = fajlok.Select(x=> x.Remove(0, ofd.FolderName.Length+1)).ToList();
+        }
+
+        private void Betolt(string neve)
+        {
+            FilterReset();
+            legok.Clear();
+            XDocument xaml = XDocument.Load(neve);
+            foreach (var elem in xaml.Descendants("Item"))
+            {
+                string id = elem.Element("ItemID").Value;
+                string nev = elem.Element("ItemName").Value;
+                string category = elem.Element("CategoryName").Value;
+                string color = elem.Element("ColorName").Value;
+                string qt = elem.Element("Qty").Value;
+
+                legok.Add(new Lego(id, nev, category, color, qt));
+            }
+            dgTablazat.ItemsSource = legok;
+            lbElemek.Content = legok.Count();
+            kategoriak = legok.Select(x => x.CategoryName).Distinct().ToList();
+            kategoriak.Add("none");
+            cbKategoriak.ItemsSource = kategoriak;
+            cbKategoriak.SelectedIndex = kategoriak.Count() - 1;
+            filtered = legok;
+        }
+        private void FilterReset()
+        {
+            tbAzon.Text = "";
+            tbNev.Text = "";
+            cbKategoriak.SelectedItem = "none";
         }
 
         private void Filters()
@@ -69,6 +87,11 @@ namespace WpfApp2
                 filtered = new ObservableCollection<Lego>(filtered.Where(x => x.ItemID.ToLower().StartsWith(tbAzon.Text.ToLower())));
             }
 
+            kategoriak.Clear();
+            kategoriak = filtered.Select(x => x.CategoryName).Distinct().ToList();
+            kategoriak.Add("none");
+            cbKategoriak.ItemsSource = kategoriak;
+
             if (cbKategoriak.SelectedIndex != kategoriak.Count() - 1)
             {
                 filtered = new ObservableCollection<Lego>(filtered.Where(x => x.CategoryName == cbKategoriak.SelectedItem));
@@ -77,10 +100,6 @@ namespace WpfApp2
             dgTablazat.ItemsSource = filtered;
 
             lbElemek.Content = filtered.Count();
-            kategoriak.Clear();
-            kategoriak = legok.Select(x => x.CategoryName).Distinct().ToList();
-            kategoriak.Add("none");
-            cbKategoriak.ItemsSource = kategoriak;
         }
 
         private void tbNev_TextChanged(object sender, TextChangedEventArgs e)
@@ -91,6 +110,16 @@ namespace WpfApp2
         private void cbKategoriak_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Filters();
+        }
+
+        private void lbxList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Betolt(lbFolder.Content.ToString() + "\\" + lbxList.SelectedItem.ToString());
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            FilterReset();
         }
     }
 }
